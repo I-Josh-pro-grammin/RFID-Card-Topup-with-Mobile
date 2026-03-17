8b0e994435d8070e989817d8fa31e17d4e20345c
IZERE JOSHUA <izerejoshua94@gmail.com>
2026-03-17 12:20:27 +0200

    updates for the mobile connection

--- mobile/constants/Config.js
+++ mobile/constants/Config.js
@@ -1,9 +1,9 @@
 import { Platform } from 'react-native';
 
-const API_BASE = 'http://127.0.0.1:5055/api';
-// Platform.OS === 'web' 
-//   ? 'http://127.0.0.1:5055/api' // For web browsers on host machine
-//   : 'http://10.84.183.218:5055/api'; // Standard for Android Emulator to host machine
+const API_BASE =
+Platform.OS === 'web' 
+  ? 'http://127.0.0.1:5055/api' // For web browsers on host machine
+  : 'http://10.11.74.117:5055/api'; // Standard for Android Emulator to host machine
 
 export default {
   API_BASE,
